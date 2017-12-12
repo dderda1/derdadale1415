64784ecffdf2c9669402184f912ed69ff830ff88
Daria Derda <dderda1@luc.edu>
2017-12-12 20:37:05 +0000

    work

---
+++ src/main/java/edu/luc/cs271/arrayqueue/Main.java
@@ -0,0 +1,19 @@
+package hw;
+import java.util.*;
+import org.jgrapht.*;
+import org.jgrapht.alg.color.GreedyColoring;
+import org.jgrapht.alg.interfaces.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.traverse.*;
+
+public class Main {
+  static final String PL = countrycodes.PL;
+  static final String DE = countrycodes.DE;
+  static final String CZ = countrycodes.CZ;
+  static final String SK = countrycodes.SK;
+  static final String UA = countrycodes.UA;
+  static final String BY = countrycodes.BY;
+  
+  public static void main(final String[] args) {
+    final Graph<String, DefaultEdge> map = new SimpleGraph<>(DefaultEdge.class);
+  
---
+++ src/main/java/edu/luc/cs271/arrayqueue/countrycodes.java
@@ -0,0 +1,18 @@
+package hw;
+import java.util.*;
+import org.jgrapht.*;
+import org.jgrapht.alg.color.GreedyColoring;
+import org.jgrapht.alg.interfaces.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.traverse.*;
+
+public class countrycodes {
+  static final String PL = "Polska";
+  static final String DE = "Niemcy";
+  static final String CZ = "Czechy";
+  static final String SK = "Slowacja";
+  static final String UA = "Ukraina";
+  static final String BY = "Bialorus";
+
+}
+ 
